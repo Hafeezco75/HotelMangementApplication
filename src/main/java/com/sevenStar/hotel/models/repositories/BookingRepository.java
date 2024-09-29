3e2b81c03416bbf4034e85d0b46d9c57ac397666
Semicolon <adedamola13@gmail.com>
2024-09-29 05:35:42 +0100

    modified guest and admin repository

--- src/main/java/com/sevenStar/hotel/models/repositories/BookingRepository.java
+++ src/main/java/com/sevenStar/hotel/models/repositories/BookingRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findByEmail(GuestUser guestUser);
 
